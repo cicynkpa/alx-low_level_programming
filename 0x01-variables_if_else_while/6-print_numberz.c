18457e40dc33cf94cc74ef229bc1bdcac1490999
Nkpadobi Chibueze <cicynkpa@yahoo.com>
2023-08-14 12:55:45 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of my codes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
