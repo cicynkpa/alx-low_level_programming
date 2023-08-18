76c2dcff0428b98f98056ff817b034fd189a4f8d
Nkpadobi Chibueze <cicynkpa@yahoo.com>
2023-08-18 11:40:15 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - func checks for digits 0 - 9
+ * Return: 0 (Success)
+*/
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 57)
+	{
+		_putchar(c);
+		i++;
+	}
+	_putchar("\n");
+}
