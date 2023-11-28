445c1f76a325174e5b71909433baa7e3b67dbfc8
Nkpadobi Chibueze <cicynkpa@yahoo.com>
2023-11-28 08:50:56 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,14 +8,18 @@
  */
 int _strlen(char *s)
 {
+
 	int i;
 
+
 	i = 0;
+
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	return (i);
 }
 
@@ -38,13 +42,18 @@ char *_strcpy(char *dest, char *src)
 	{
 		len++;
 	}
+
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
-	return (dest);
+		dest[i] = '\0';
+
+
+		return (dest);
 }
+
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -55,22 +64,25 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	 dog_t *dog;
-	 int len1, len2;
+	dog_t *dog;
+	int len1, len2;
 
-	 len1 = _strlen(name);
-	 len2 = _strlen(owner);
 
-	 dog = malloc(sizeof(dog_t));
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	return (NULL);
-	 dog->name = malloc(sizeof(char) * (len1 + 1));
+		return (NULL);
+
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
-	free(dog);
-	return (NULL);
+		free(dog);
+		return (NULL);
 	}
-}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
@@ -81,6 +93,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
+
 
 	return (dog);
 }
